9add629e866052194233ae1d1927ce4cc077e00f
Motekema <motekema@gmail.com>
2023-10-31 01:59:11 -0700

    My commit

---
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby
+
+# Check if the argument matches the regular expression /School/
+if ARGV[0] =~ /School/
+  # If there is a match, print the matched string
+  puts ARGV[0].scan(/School/).join
+else
+  # If there is no match, print an empty line
+  puts ""
+end
+
