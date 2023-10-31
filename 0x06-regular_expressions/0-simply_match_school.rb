bf5bbcfd2e3c51fedde510a4fefe9892e44d917f
Motekema <motekema@gmail.com>
2023-10-31 03:04:26 -0700

    My commit

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
-
 # Check if the argument matches the regular expression /School/
+
 if ARGV[0] =~ /School/
   # If there is a match, print the matched string
   puts ARGV[0].scan(/School/).join
