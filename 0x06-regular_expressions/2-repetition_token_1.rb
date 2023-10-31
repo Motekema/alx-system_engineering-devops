fbc04db78717a8d2ea00e30754d338914885dd8b
Motekema <motekema@gmail.com>
2023-10-31 05:27:46 -0700

    My commit

--- 0x06-regular_expressions/2-repetition_token_1.rb
+++ 0x06-regular_expressions/2-repetition_token_1.rb
@@ -1,8 +1,4 @@
 #!/usr/bin/env ruby
 # Check if the argument matches the regular expression
 
-if ARGV[0] =~ /hbt*n/
-  # If there is a match, print the matched string
-  puts ARGV[0]
-end
-
+puts ARGV[0].scan(/hb?t?n/).join
