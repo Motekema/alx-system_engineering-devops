487cdcb3b3e430ae8d1b4601e62095c1a026c2b8
Motekema <motekema@gmail.com>
2023-12-22 04:23:23 -0800

    My commit

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,9 @@
 # Puppet manifest to create a file in /tmp
 file { 'school':
-  mode       => '0744',
-  owner      => 'www-data',
-  group      => 'www-data',
-  content    => 'I love Puppet',
+  ensure  => 'present',
+  content => 'I love Puppet',
+  group   => 'www-data',
+  mode    => '0744',
+  owner   => 'www-data',
+  path    => '/tmp/school',
 }
-
