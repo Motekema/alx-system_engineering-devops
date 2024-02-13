736df0ba4319a997dacbd05ad630ec2df8e93639
T.K Mahlanya <91951223+Motekema@users.noreply.github.com>
2024-02-13 12:05:25 +0200

    My commit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,21 +4,21 @@
 """
 
 import requests
+from sys import argv
+
 
 def number_of_subscribers(subreddit):
-    """
-    Returns the number of subscribers for a given subreddit
-    """
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'Custom User Agent'}  # Ensure a custom User-Agent header is set
-
-    response = requests.get(url, headers=headers)
-    if response.status_code == 200:
-        data = response.json()
-        return data['data']['subscribers']
-    else:
+    '''
+        Displays the number of subscribers for a given subreddit
+    '''
+    user = {'User-Agent': 'Lizzie'}
+    url = requests.get('https://www.reddit.com/r/{}/about.json'
+                       .format(subreddit), headers=user).json()
+    try:
+        return url.get('data').get('subscribers')
+    except Exception:
         return 0
 
+
 if __name__ == "__main__":
-    number_of_subscribers = number_of_subscribers("programming")
-    print(number_of_subscribers)
+    number_of_subscribers(argv[1])
