5511c51e7c79a328acf1f142e286b4d0a073401e
T.K Mahlanya <91951223+Motekema@users.noreply.github.com>
2024-02-14 17:03:18 +0200

    Commit

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""
-2-recurse
-"""
+"""2-recurse"""
 import requests
 
 
@@ -9,8 +7,7 @@ def recurse(subreddit, hot_list=[], after="", count=0):
     """Displays a list of titles of all hot posts on a given subreddit."""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
     headers = {
-        "User-Agent": "0x16-api_advanced:project:\
-v1.0.0 (by /u/firdaus_cartoon_jr)"
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
     params = {
         "after": after,
